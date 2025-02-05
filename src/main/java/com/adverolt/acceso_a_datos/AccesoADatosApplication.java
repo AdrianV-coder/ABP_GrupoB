988fc3f2ccbe1043a411d147413b18353e846a71
Adrián Vernich <vernicholtraadrian@gmail.com>
2025-02-05 08:57:06 +0100

    Primera subida archivos (Acceso a Datos) <3

---
+++ src/main/java/com/adverolt/acceso_a_datos/AccesoADatosApplication.java
@@ -0,0 +1,13 @@
+package com.adverolt.acceso_a_datos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoADatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesoADatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adverolt/acceso_a_datos/AccesoADatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adverolt.acceso_a_datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoADatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
